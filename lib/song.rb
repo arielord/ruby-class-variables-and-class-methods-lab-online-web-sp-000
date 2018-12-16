c9c45c920ffb79eff3e292511b8463b87b9fe974
Ariel <arielord@buffalo.edu>
2018-12-16 22:41:12 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,33 @@
+class Song
+  attr_accessor song, artist, genre
+  @@genres = []
+  @@artists = []
+  @@count = 0
+  
+  def initialize(song, artist, genre)
+    @@song_count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.genre_count
+    @@genres.uniq.length
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.artist_count
+    @@artists.uniq.length
+  end
+  
+end
